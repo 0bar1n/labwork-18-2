a10986155acc1d981164781164e02ad5d49d232b
barinskaya <1388zxc@gmail.com>
2022-12-29 10:21:14 +0300

    Добавьте файлы проекта.

---
+++ labwork 18 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace labwork_18_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            RevStr letter = new RevStr();//функция реверсии
+            letter.DisplayRev("abc"); //задание строки
+            Console.ReadLine(); //прорамма ждет нажатия клавиши для завершения работы
+        }
+    }
+    class RevStr
+    {
+
+        public void DisplayRev(string str) 
+        {
+            if (str.Length > 0) //выполнять, пока в строке не останется символов
+                DisplayRev(str.Substring(1, str.Length - 1)); //выполнить реверсию
+            else//другой исход
+                return;//повторить реверсию
+
+            Console.WriteLine(str[0]); //вывод символов строки в обратном порядке
+        }
+    }
+}
